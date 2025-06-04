cf29e2020a863cd275f9a66488fd51c28b728228
burna money <agborboniventure@gmail.com>
2025-06-04 16:45:49 +0100

    my  commit9

--- src/component/home/HomeComponent.jsx
+++ src/component/home/HomeComponent.jsx
@@ -29,7 +29,7 @@ const HomeComponent = () => {
     <div className='w-full h-fit  md:h-[600px] overflow-y-auto '>
      
      <div className="fixed z-20 bg-background w-[500px] md:w-full ">
-   <div className='flex justify-between w-[300px] md:w-full py-4 px-2  bg-background  '>
+   <div className='flex justify-between w-[390px] md:w-[600px] bg-background py-4 px-2 '>
       <div>Home</div>
       <div className='flex gap-2 justify-center align-center'>
          <div className='md:hidden '><MenuBar/></div>
